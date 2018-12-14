316c4cc2f858a11e34f851bf576b3cad2baa460d
Alex Hoyau <a.hoyau@silexlabs.org>
2018-12-14 17:13:22 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 
 $(function() {
     var siteWidth = $('meta[name=website-width]').attr('content');
+    function doResize() {
+        var viewportWidth = $(window).width();
+        if(viewportWidth > 480 && viewportWidth < siteWidth) {
+            $('body').css({
+              'transform': 'scale(' + (viewportWidth / 1200) + ')',
+              'transform-origin': '0 0',
+            })
+        }
+        else {
+            $('body').css({
+              'transform': '',
+              'transform-origin': '',
+            })
+        }
+    }
     if(siteWidth) {
-        $(window).resize(function() {
-            var viewportWidth = $(window).width();
-            if(viewportWidth > 480 && viewportWidth < siteWidth) {
-                $('body').css({
-                  'transform': 'scale(' + (viewportWidth / 1200) + ')',
-                  'transform-origin': '0 0',
-                })
-            }
-            else {
-                $('body').css({
-                  'transform': '',
-                  'transform-origin': '',
-                })
-            }
-        })
+        $(window).resize(doResize);
+        doResize();
     }
 });
